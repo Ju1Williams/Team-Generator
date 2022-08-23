173d7953f878e768b72285ae16879ef20d3405e6
Julian <williamsmjulian@gmail.com>
2022-08-23 10:33:53 -0400

    File Organization

--- index.js
+++ index.js
@@ -1,4 +1,41 @@
-require = inquirer('');
-const fs = require;
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Manager = require ('./Manager');
+const Employee = require ('./Employee');
+const Engineer = require ('./Engineer');
+const path = require("path");
 
-prompt 
+
+
+const promptEmployee = () => {
+  return inquierer.prompt ([
+    {
+      type: "choice",
+      name: ""
+    }
+  ])
+}
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'checkbox',
+      message: 'What languages do you know?',
+      name: 'stack',
+      choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+    },
+    {
+      type: 'list',
+      message: 'What is your preferred method of communication?',
+      name: 'contact',
+      choices: ['email', 'phone', 'telekinesis'],
+    },
+  ])
+
+
+    fs.writeFileSync
