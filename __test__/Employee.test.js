50607fefcdbcd858ba684a0a265ef710805e62cb
Julian <williamsmjulian@gmail.com>
2022-08-24 09:03:44 -0400

    Making sure test pass

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,3 +1,23 @@
-const Employee = require ('../lib/Employee');
+const Employee = require ('../lib/employee');
+const employee = new Employee('Steph','2', 'stephcurry@gmail')
+
+describe("Employee", () => {
+    describe('name', () => {
+        it('Get name', () => {
+            expect(employee.name).toEqual('Steph')
+        })
+    })
+    describe('id', () => {
+        it('Get id', () => {
+            expect(employee.id).toEqual('2')
+        })
+    })
+    describe('email', () => {
+        it('Get email', () => {
+            expect(employee.email).toEqual('stephcurry@gmail')
+        })
+    })
+    
+})
 
 
--- __test__/Engineer.test.js
+++ __test__/Engineer.test.js
@@ -1,29 +1,25 @@
-const Engineer = require('../lib/Engineer');
-const engineer = new Engineer('Sandra','2001', 'sandrabullock@gmail','sandyb' );
+const Engineer = require('../lib/engineer');
+const engineer = new Engineer('Sandra','1', 'sandrabullock@gmail','sandyb' );
 
-test('make sure they run', () => {
-    expect(engineer.name).toBe('Sandra');
-    expect(engineer.id).toEqual(expect.any(Number));
-    expect(engineer.email).toEqual('sandrabullock@gmail');
-    expect(engineer.github).toEqual('sandyb');
-
+describe("Engineer", () => {
+    describe('name', ()=> {
+        it('Get name', () => {
+            expect(engineer.name).toEqual('Sandra')
+        })
+    });
+    describe('id', ()=> {
+        it('Get 2001', () => {
+            expect(engineer.id).toEqual('1')
+        })
+    });
+    describe('email', ()=> {
+        it('Get email', () => {
+            expect(engineer.email).toEqual('sandrabullock@gmail')
+        })
+    });
+    describe('github', ()=> {
+        it('Get github', () => {
+            expect(engineer.github).toEqual('sandyb')
+        })
+    });
 });
-    
-
-test("name", () => {
-    expect(engineer.getName()).toBe('Sandra');
-});
-
-test("id", () => {
-    expect(engineer.getId()).toBe('2001');
-});
-
-test("email", () => {
-    expect(engineer.getEmail()).toBe('sandrabullock@gmail');
-});
-
-test("github", () => {
-    expect(engineer.getGithub()).toBe('sandyb');
-});
-
-
